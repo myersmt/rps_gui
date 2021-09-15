9904818b07d79ecccad67c8fe06b7f6756939baf
myersmt <myersmt@pm.me>
2021-09-15 17:31:40 -0400

    simplify wins track

--- rps_func.py
+++ rps_func.py
@@ -12,26 +12,26 @@ def results(uc, cc):
     elif uc == 'Rock':
         if cc == 'Scissors':
             message = 'You\'ve won, Congratulations!'
-            u += 1
+            u = 1
         else:
             message = 'You\'ve lost, better luck next time!'
-            c += 1
+            c = 1
         return [message,u,c];
     elif uc == 'Paper':
         if cc == 'Rock':
             message = 'You\'ve won, Congratulations!'
-            u += 1
+            u = 1
         else:
             message = 'You\'ve lost, better luck next time!'
-            c += 1
+            c = 1
         return [message,u,c];
     elif uc == 'Scissors':
         if cc == 'Paper':
             message = 'You\'ve won, Congratulations!'
-            u += 1
+            u = 1
         else:
             message = 'You\'ve lost, better luck next time!'
-            c += 1
+            c = 1
         return [message,u,c];
     else:
         message = 'Error'
